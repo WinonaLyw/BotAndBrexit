2b1147c8544cbe2b73a9d8a688544c122f37519d
winona <winona0428@sina.com>
2020-02-27 22:01:20 +0000

    1. tweets analysis, hourly count 2. save daily count, daily difference, hourly count plots

--- select_tweets.py
+++ select_tweets.py
@@ -17,10 +17,18 @@ tweets['created_date'] = [t.split(' ')[0] for t in tweets.created]
 # groupby date to get daily tweets count from 1 Jan to 31 Mar 2018
 d_counts = tweets[['created_date', 'created']].groupby('created_date').count()
 
-# plot daily tweets count
-d_counts['created'].plot()
 # list top 10 
 d_counts.nlargest(10, 'created')
+
+# %% plot daily tweets count
+plt.plot(d_counts.index, d_counts['created'])
+plt.title('No. of #Brexit tweets per day from 1 Jan to 31 Mar, 2018')
+plt.xlabel('date')
+plt.legend(labels=['tweet count'])
+plt.xlim(0, len(d_counts)-1)
+plt.xticks([0, 14, 31, 45, 59, 73, 89], rotation=20)
+plt.savefig('daily_dist.png')
+
 
 # %% daily difference
 # calculate daily difference comparing the previous day
@@ -38,7 +46,14 @@ for i in range(1, len(dates)):
 d_counts.nlargest(10, 'moving_diff')
 d_counts.nsmallest(10, 'moving_diff')
 
-d_counts['moving_diff'].plot()
+# %% plot daily tweets count
+plt.plot(d_counts.index, d_counts['moving_diff'])
+plt.title('Difference of #Brexit tweets to previous day from 1 Jan to 31 Mar, 2018')
+plt.xlabel('date')
+plt.legend(labels=['tweet count difference'])
+plt.xlim(0, len(d_counts)-1)
+plt.xticks([0, 14, 31, 45, 59, 73, 89], rotation=20)
+plt.savefig('daily_diff_dist.png')
 
 # ================================
 # 2018-03-29 
@@ -51,4 +66,3 @@ d_counts['moving_diff'].plot()
 tweets_s = tweets[tweets.created_date == '2018-03-29']
 tweets_s.to_csv('sample_tweets.csv')
 
-# %%
--- tweets_analysis.py
+++ tweets_analysis.py
@@ -1,30 +1,22 @@
 # %% import
 import numpy as np
 import pandas as pd
+import re
+import matplotlib.pyplot as plt
 from nltk.tokenize import TweetTokenizer
 
-# %% read tweets
-tweets = pd.read_csv("Tweets1.267Million.csv" , encoding='latin-1')
-tweets.head(4)
+# %% read sample tweets
+tweets_s = pd.read_csv("sample_twwets.csv")
+tweets_s.head(4)
 
-# %% read terget accounts
-human = pd.read_csv("human_accounts.csv")
-bots = pd.read_csv("bots_accounts.csv")
+# %% hourly tweets count
+tweets_s['hour'] = [re.split('[ :]', t)[1] for t in tweets_s.created]
 
-# %% concatenate two account dataframes
-accounts = pd.concat([human, bots], ignore_index=True)
+hourly_dist = tweets_s[['hour', 'created']].groupby('hour').count()
 
-accounts.head(2)
-# %% select tweets tweeted by sample accounts
-acc_tweets = tweets[tweets['screenName'].isin(accounts.user)]
-print (len(acc_tweets))
-
-# %%
-tkznr = TweetTokenizer()
-acc_tweets['text_token'] = acc_tweets.apply(lambda row: tknzr.tokenize(row['text']), axis=1)
-acc_tweets.head(4)
-
-# %%
-acc_tweets.to_csv("sample_tweets.csv")
-
+# %% plot hourly dist
+hourly_dist.plot()
+plt.title('No. of #Brexit tweets per hour on 29 Mar, 2018')
+plt.legend(labels=['tweet count'])
+plt.savefig('hourly_dist.png')
 # %%
